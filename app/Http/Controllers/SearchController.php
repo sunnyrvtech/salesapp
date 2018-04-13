b3e61fda9139edbc46d4bb53529b1354814836ea
sunny <sunny_kumar@rvtechnologies.co.in>
2018-04-13 10:54:00 +0530

    first commit

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
+use App\Product;
+
+class SearchController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        //return view ( 'searchresult' )->withDetails($products)->withQuery ($keyword);
+        $keyword = Input::get ( 'q' );
+        if(isset($keyword) OR $keyword!=""){
+        $products = Product::where('sku','LIKE',"%{$keyword}%")
+                     ->orWhere("name", "LIKE", "%{$keyword}%")->paginate(10);  
+        if (count ( $products ) > 0){
+             return view ( 'home' )->withDetails($products)->withQuery ($keyword);
+        }else{
+             return view ( 'home' )->withMessage('No Details found. Try to search again !');
+        }
+      }else{
+            return view ( 'home' )->withMessage('Please enter Keyword');
+      }
+                   
+    }
+      public function view()
+    {
+        $keyword = Input::get ( 'q' );
+        if(isset($keyword) OR $keyword!=""){
+        
+        $product = Product::find($keyword);
+                     
+        if( $product){
+             return view ( 'detail' )->withDetails($product);
+        }else{
+             return view ( 'detail' )->withMessage('No Details found. Try to search again !');
+        }
+      }else{
+            return view ( 'detail' )->withMessage('No Record');
+      }
+    }
+}
---
+++ database/migrations/2018_04_11_024822_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sku');
+            $table->string('brand');
+            $table->string('name');
+            $table->string('msrp');
+            $table->string('price');
+            $table->string('cost');
+            $table->string('ship_cost');
+            $table->text('options');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
